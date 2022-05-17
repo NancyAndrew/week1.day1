8e7e3d4aad2aed664c460e8aa254c79cfb888504
NancyAndrew <105703194+NancyAndrew@users.noreply.github.com>
2022-05-17 12:58:54 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Mobile {
+	
+public static void makeCall () {
+	System.out.println("Make Call");
+	
+}
+public static void sendMessage() {System.out.println("Send Message");}
+
+
+	
+public static void main(String[] args) {
+	System.out.println("Inside Main Method");
+
+Mobile.makeCall();
+Mobile.sendMessage();
+
+}
+
+}
+
+
+
+
