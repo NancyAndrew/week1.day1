1c5192172d0bae104aef9ca88433bc8c7989a28d
NancyAndrew <105703194+NancyAndrew@users.noreply.github.com>
2022-05-22 15:44:49 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+
+	
+	
+	 int noOfWheeles =2;
+	 short noOfMirrors = 2;
+	 long chassisNumber = 2876546787L;
+	 boolean isPunctured = false;
+	 String bikeName = "Fascino 125";
+	 double runningKM = 6000;
+	 
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TwoWheeler obj = new TwoWheeler();
+		
+		System.out.println("No of Wheels = " +obj.noOfWheeles);
+		System.out.println("No of Mirrors = " +obj.noOfMirrors);
+		System.out.println("Chassis Number = " +obj.chassisNumber);
+		System.out.println("Is Punctured = " +obj.isPunctured);
+		System.out.println("Bike Name = " +obj.bikeName);
+		System.out.println("Running KM = " +obj.runningKM);
+		
+		
+		
+		
+
+	}
+
+}
